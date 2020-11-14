26a21e440cd843d2cb7cd8a21dc0aeb5284933e0
vaibhav mehra <vaibhavmehra@vaibhavs-MacBook-Air.local>
2020-11-14 17:41:55 +0530

    navbar changes added

---
+++ src/components/Nav/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavStyle from "./style";
+
+const Nav = () => {
+  return (
+    <NavStyle>
+      <div className="nav-container">
+        <h4>Meal Tracker</h4>
+      </div>
+    </NavStyle>
+  );
+};
+
+export default Nav;
---
+++ src/components/Nav/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const NavStyle = styled.nav`
+  border-bottom: 1px solid #efefef;
+  height: 70px;
+  .nav-container {
+    height: 100%;
+    width: 100%;
+    max-width: 1170px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export default NavStyle;
