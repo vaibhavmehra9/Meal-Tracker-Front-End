3234027803ffc59eca44ca9f18a3b8cba69b54fd
vaibhav mehra <vaibhavmehra@vaibhavs-MacBook-Air.local>
2020-11-15 21:45:02 +0530

    close btn added in popup

--- src/components/CommonPopUp/index.js
+++ src/components/CommonPopUp/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import PopUp from "reactjs-popup";
-import Button from "../Button";
 import { connect } from "react-redux";
 import * as popUpActions from "../../store/actions/popUpActions";
+import { GrClose } from "react-icons/gr";
 
 const CommonPopUp = ({ popup: { open }, closePopUp, children }) => {
   return (
     <PopUp open={open} modal closeOnDocumentClick={false}>
       <div className="popup">
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <GrClose onClick={closePopUp} style={{ cursor: "pointer" }} />
+        </div>
         <div className="popup-content">{children}</div>
       </div>
     </PopUp>
